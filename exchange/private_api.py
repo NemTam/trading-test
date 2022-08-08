b8f484f76e4523f60b8cc3550d21de501c40a358
Tamas Nemeth <tamas.nemeth@bvtech.io>
2022-08-08 21:53:34 +0200

    type hints added

--- exchange/private_api.py
+++ exchange/private_api.py
@@ -6,11 +6,12 @@ import urllib.parse
 
 import pyotp
 import requests
+from requests.models import Response
 
 from exchange.constants import API_URL, API_KEY, PRIVATE_KEY, OTP_SEED
 
 
-def get_api_signature(urlpath, data, secret):
+def get_api_signature(urlpath: str, data: dict, secret: str) -> str:
     postdata = urllib.parse.urlencode(data)
     encoded = (str(data['nonce']) + postdata).encode()
     message = urlpath.encode() + hashlib.sha256(encoded).digest()
@@ -19,20 +20,18 @@ def get_api_signature(urlpath, data, secret):
     return sigdigest.decode()
 
 
-def send_post_request(uri_path, data):
-    headers = {}
-    headers['API-Key'] = API_KEY
-    headers['API-Sign'] = get_api_signature(uri_path, data, PRIVATE_KEY)
+def send_post_request(uri_path: str, data: dict) -> Response:
+    headers = {'API-Key': API_KEY, 'API-Sign': get_api_signature(uri_path, data, PRIVATE_KEY)}
     req = requests.post((API_URL + uri_path), headers=headers, data=data)
     return req
 
 
-def generate_otp():
+def generate_otp() -> str:
     totp = pyotp.TOTP(OTP_SEED)
-    return totp.now()  # => '492039'
+    return totp.now()
 
 
-def get_open_orders():
+def get_open_orders() -> Response:
     resp = send_post_request('/0/private/OpenOrders', {
         "nonce": str(int(1000 * time.time())),
         "otp": generate_otp(),
--- exchange/public_api.py
+++ exchange/public_api.py
@@ -1,18 +1,17 @@
 import requests
+from requests.models import Response
 
 from exchange.constants import API_URL
 
 
-def get_server_time():
+def get_server_time() -> Response:
     response = requests.get(f"{API_URL}/0/public/Time")
     return response
 
 
-def get_pricing_pair(pair: list[str], info: str = None):
+def get_pricing_pair(pair: list[str], info: str = None) -> Response:
     param = {"pair": ",".join(pair)}
     if info:
         param["info"] = info
     resp = requests.get(f"{API_URL}/0/public/AssetPairs", params=param)
     return resp
-
-
